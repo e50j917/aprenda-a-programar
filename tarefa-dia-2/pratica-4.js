d0c81fb7e2302a9f2a647dc193483ea576b044e7
e50j917 <israelesoj@gmail.com>
2022-08-29 11:50:59 -0300

    Corrigindo erro, passando codigo de pratica-4 para pratica-2

---
+++ tarefa-dia-2/pratica-4.js
@@ -0,0 +1,11 @@
+
+let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true};
+// let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true};
+// let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false};
+
+if (produtoA == "internacional") {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
