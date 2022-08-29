6fa8f98b96e60aca57b4ad1a53ab950ec9bd0699
e50j917 <israelesoj@gmail.com>
2022-08-28 22:08:53 -0300

    Editando o arquivo pratica-1.js

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,21 @@
+
+    var meuNome = "israel";
+    var meuEmail = "duck@yahoo.com";
+    var profissao = "lenhador";
+    var idade = 29;
+    var viagemDosSonhos = "Chapada Diamantina";
+
+    function formulario() {
+        console.log(meuNome, meuEmail, profissao, idade, viagemDosSonhos);
+
+    }
+
+    formulario();
+    
+    
+    
+
+
+
+
+
