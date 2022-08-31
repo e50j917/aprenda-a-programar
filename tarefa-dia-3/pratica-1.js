57fdd1e11d6f651206af56a6772f58f3535605a2
Israel <israelesoj@gmail.com>
2022-08-31 15:27:23 -0300

    Codando o arquivo pratica-1.js

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,11 @@
+
+var contador = 1;
+while (contador <= 9);
+    //%d\n pula linha quando operado...
+    {
+        console.log("contador = %d\n", contador);
+        contador += 1;
+
+    }
+
+console.log("FIM!!!\n");
